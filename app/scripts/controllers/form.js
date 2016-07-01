df6c9701f660228f10194000a8cc72313c2ed1ce
Antonio Rodríguez <antonio.rodriguez@outbook.es>
2016-07-01 14:52:48 +0200

    añadido formulario

--- app/scripts/controllers/form.js
+++ app/scripts/controllers/form.js
@@ -9,5 +9,19 @@
  */
 moduleAngular
   .controller('FormCtrl', ['$scope', function ($scope) {
+    $scope.master = {};
 
+    $scope.update = function(user) {
+      $scope.master = angular.copy(user);
+    };
+
+    $scope.reset = function(form) {
+      if (form) {
+        form.$setPristine();
+        form.$setUntouched();
+      }
+      $scope.user = angular.copy($scope.master);
+    };
+
+    $scope.reset();
   }]);
