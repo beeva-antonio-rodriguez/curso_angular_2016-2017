7c99000181694c01a929914509f2eb04e3127a07
Antonio Rodríguez <antonio.rodriguez@outbook.es>
2016-07-30 23:01:18 +0200

    validaciones

--- app/scripts/controllers/form.js
+++ app/scripts/controllers/form.js
@@ -7,7 +7,21 @@
  * # AboutCtrl
  * Controller of the cursoAngular20162017App
  */
-moduleAngular
-  .controller('FormCtrl', ['$scope', function ($scope) {
 
-  }]);
+moduleAngular.controller('FormCtrl', ['$scope', function($scope) {
+  $scope.master = {};
+
+  $scope.update = function(user) {
+    $scope.master = angular.copy(user);
+  };
+
+  $scope.reset = function(form) {
+    if (form) {
+      form.$setPristine();
+      form.$setUntouched();
+    }
+    $scope.user = angular.copy($scope.master);
+  };
+
+  $scope.reset();
+}]);
